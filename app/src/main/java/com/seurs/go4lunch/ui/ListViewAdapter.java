90e6d358f3916178646471f3f9f34f104d52ebc2
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-18 16:31:32 +0200

    Update ListViewAdapter.java

--- app/src/main/java/com/seurs/go4lunch/ui/ListViewAdapter.java
+++ app/src/main/java/com/seurs/go4lunch/ui/ListViewAdapter.java
@@ -1,6 +1,6 @@
 package com.seurs.go4lunch.ui;
 
-import android.view.View;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -8,9 +8,14 @@ import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.seurs.go4lunch.databinding.ListRowItemBinding;
 import com.seurs.go4lunch.domain.model.Restaurant;
 
 public class ListViewAdapter extends ListAdapter<Restaurant, ListViewAdapter.ListViewHolder> {
+
+    private ListRowItemBinding binding;
 
     protected ListViewAdapter(@NonNull DiffUtil.ItemCallback<Restaurant> diffCallback) {
         super(diffCallback);
@@ -19,17 +24,32 @@ public class ListViewAdapter extends ListAdapter<Restaurant, ListViewAdapter.Lis
     @NonNull
     @Override
     public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        binding = ListRowItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new ListViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
-
+        Restaurant restaurant = getItem(position);
+        holder.bind(restaurant);
     }
 
     class ListViewHolder extends RecyclerView.ViewHolder {
-        public ListViewHolder(@NonNull View itemView) {
-            super(itemView);
+        private ListRowItemBinding binding;
+
+        public ListViewHolder(@NonNull ListRowItemBinding itemBinding) {
+            super(itemBinding.getRoot());
+            this.binding = itemBinding;
+        }
+
+        void bind(Restaurant restaurant) {
+            Glide.with(binding.photo.getContext()).load(restaurant.photoUrl).transform(new CenterCrop()).into(binding.photo);
+            binding.name.setText(restaurant.name);
+            binding.address.setText(restaurant.address);
+            binding.opening.setText(restaurant.opening);
+            binding.distance.setText(restaurant.distance);
+            binding.workmates.setText(restaurant.workmates);
+            binding.ratingBar.setRating(restaurant.rating);
         }
     }
 }
