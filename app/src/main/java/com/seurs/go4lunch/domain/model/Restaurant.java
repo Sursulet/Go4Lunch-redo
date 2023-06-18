bbd2069729d551703b5610161162ce6e422f8fa0
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-18 16:28:11 +0200

    Update Restaurant.java

--- app/src/main/java/com/seurs/go4lunch/domain/model/Restaurant.java
+++ app/src/main/java/com/seurs/go4lunch/domain/model/Restaurant.java
@@ -1,4 +1,11 @@
 package com.seurs.go4lunch.domain.model;
 
 public class Restaurant {
+    public String name;
+    public String photoUrl;
+    public String address;
+    public String opening;
+    public String distance;
+    public float rating;
+    public String workmates;
 }
