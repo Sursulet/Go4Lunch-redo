53d36d2e77d59c570c2943c1e8da308827e4392e
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-23 16:59:35 +0200

    Create AddressComponentDto.java

---
+++ app/src/main/java/com/seurs/go4lunch/data/remote/places_api/dto/AddressComponentDto.java
@@ -0,0 +1,9 @@
+package com.seurs.go4lunch.data.remote.places_api.dto;
+
+import java.util.List;
+
+public class AddressComponentDto {
+    private String long_name;
+    private String short_name;
+    private List<String> types;
+}
