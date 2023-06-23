5d896f90243878214a1105b701350717110a06da
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-23 16:54:01 +0200

    Create PlaceReviewDto.java

---
+++ app/src/main/java/com/seurs/go4lunch/data/remote/places_api/dto/PlaceReviewDto.java
@@ -0,0 +1,14 @@
+package com.seurs.go4lunch.data.remote.places_api.dto;
+
+public class PlaceReviewDto {
+    private String author_name;
+    private String rating;
+    private String relative_time_description;
+    private int time;
+    private String author_url;
+    private String language;
+    private String original_language;
+    private String profile_photo_url;
+    private String text;
+    private boolean translated;
+}
