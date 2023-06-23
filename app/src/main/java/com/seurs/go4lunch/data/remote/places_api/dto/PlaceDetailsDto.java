88757ab25409ed99ee158b43b907edf6965938bb
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-23 16:58:52 +0200

    Create PlaceDetailsDto.java

---
+++ app/src/main/java/com/seurs/go4lunch/data/remote/places_api/dto/PlaceDetailsDto.java
@@ -0,0 +1,10 @@
+package com.seurs.go4lunch.data.remote.places_api.dto;
+
+import java.util.List;
+
+public class PlaceDetailsDto {
+    private List<String> html_attributions;
+    private PlaceDto result;
+    private PlacesDetailsStatusDto status;
+    private List<String> info_messages;
+}
