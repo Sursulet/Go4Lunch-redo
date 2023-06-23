7bd814e702a3e5fcba9d0d946bef84f148abb1b5
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-23 10:37:32 +0200

    Update MainActivity.java

--- app/src/main/java/com/seurs/go4lunch/MainActivity.java
+++ app/src/main/java/com/seurs/go4lunch/MainActivity.java
@@ -1,14 +1,37 @@
 package com.seurs.go4lunch;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+import com.seurs.go4lunch.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.topAppBar.setNavigationOnClickListener(view -> mBinding.drawer.open());
+        mBinding.navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
+
+        mBinding.bottomNavigation.setOnItemReselectedListener(new NavigationBarView.OnItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+
+            }
+        });
     }
 }
