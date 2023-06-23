5a1752df623ca763805a0817466facc8f4edf722
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-23 16:58:27 +0200

    Create PlaceOpeningHoursPeriodDetailDto.java

---
+++ app/src/main/java/com/seurs/go4lunch/data/remote/places_api/dto/PlaceOpeningHoursPeriodDetailDto.java
@@ -0,0 +1,8 @@
+package com.seurs.go4lunch.data.remote.places_api.dto;
+
+public class PlaceOpeningHoursPeriodDetailDto {
+    private int day;
+    private String time;
+    private String date;
+    private boolean truncated;
+}
