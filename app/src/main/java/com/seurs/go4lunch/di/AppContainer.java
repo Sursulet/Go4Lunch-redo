d3fd59f90c51f3b14638e38789b9e18c31e03a8d
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-23 18:22:53 +0200

    Update AppContainer.java

--- app/src/main/java/com/seurs/go4lunch/di/AppContainer.java
+++ app/src/main/java/com/seurs/go4lunch/di/AppContainer.java
@@ -3,6 +3,8 @@ package com.seurs.go4lunch.di;
 import static com.seurs.go4lunch.util.Constants.BASE_URL;
 
 import com.seurs.go4lunch.data.remote.places_api.PlacesApi;
+import com.seurs.go4lunch.data.repository.NearbySearchRepositoryImpl;
+import com.seurs.go4lunch.domain.repository.NearbySearchRepository;
 
 import retrofit2.Retrofit;
 
@@ -22,4 +24,8 @@ public class AppContainer {
             .baseUrl(BASE_URL)
             .build()
             .create(PlacesApi.class);
+
+    private NearbySearchRepository getNearbySearchRepository() {
+        return new NearbySearchRepositoryImpl(placesApi);
+    }
 }
