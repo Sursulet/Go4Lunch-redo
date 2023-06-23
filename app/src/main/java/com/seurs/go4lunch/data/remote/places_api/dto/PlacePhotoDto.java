8b6ced8e4c0a871fa2e92eaecf23c234983f550e
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-23 16:58:16 +0200

    Create PlacePhotoDto.java

---
+++ app/src/main/java/com/seurs/go4lunch/data/remote/places_api/dto/PlacePhotoDto.java
@@ -0,0 +1,10 @@
+package com.seurs.go4lunch.data.remote.places_api.dto;
+
+import java.util.List;
+
+public class PlacePhotoDto {
+    private int height;
+    private List<String> html_attributions;
+    private String photo_reference;
+    private int width;
+}
