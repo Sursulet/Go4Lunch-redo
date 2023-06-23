cdbfb09e05c65b52940864e5dadeb11fdb65e7de
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-23 16:58:22 +0200

    Create PlaceOpeningHoursPeriodDto.java

---
+++ app/src/main/java/com/seurs/go4lunch/data/remote/places_api/dto/PlaceOpeningHoursPeriodDto.java
@@ -0,0 +1,6 @@
+package com.seurs.go4lunch.data.remote.places_api.dto;
+
+public class PlaceOpeningHoursPeriodDto {
+    private PlaceOpeningHoursPeriodDetailDto open;
+    private PlaceOpeningHoursPeriodDetailDto close;
+}
