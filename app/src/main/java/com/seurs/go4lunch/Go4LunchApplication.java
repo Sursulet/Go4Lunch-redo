44dbfb88bb735e164b14f7eafcf8c28c0c84b1d1
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-24 18:36:30 +0200

    Update Go4LunchApplication.java

--- app/src/main/java/com/seurs/go4lunch/Go4LunchApplication.java
+++ app/src/main/java/com/seurs/go4lunch/Go4LunchApplication.java
@@ -2,16 +2,24 @@ package com.seurs.go4lunch;
 
 import android.app.Application;
 
+import com.seurs.go4lunch.di.AppContainer;
+
 public class Go4LunchApplication extends Application {
     private static Application sApplication;
+    private static AppContainer sAppContainer;
 
     @Override
     public void onCreate() {
         super.onCreate();
         sApplication = this;
+        sAppContainer = new AppContainer();
     }
 
     public static Application getApplication() {
         return sApplication;
+    }
+
+    public static AppContainer getAppContainer() {
+        return sAppContainer;
     }
 }
