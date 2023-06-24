3f8c82843c6cfb08c2b8a4466ee56889d41f41ee
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-24 18:41:57 +0200

    Update AppContainer.java

--- app/src/main/java/com/seurs/go4lunch/di/AppContainer.java
+++ app/src/main/java/com/seurs/go4lunch/di/AppContainer.java
@@ -2,30 +2,29 @@ package com.seurs.go4lunch.di;
 
 import static com.seurs.go4lunch.util.Constants.BASE_URL;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.seurs.go4lunch.Go4LunchApplication;
+import com.seurs.go4lunch.data.location.DefaultLocationTracker;
 import com.seurs.go4lunch.data.remote.places_api.PlacesApi;
 import com.seurs.go4lunch.data.repository.NearbySearchRepositoryImpl;
+import com.seurs.go4lunch.data.repository.PlaceDetailsRepositoryImpl;
+import com.seurs.go4lunch.domain.location.LocationTracker;
 import com.seurs.go4lunch.domain.repository.NearbySearchRepository;
+import com.seurs.go4lunch.domain.repository.PlaceDetailsRepository;
 
 import retrofit2.Retrofit;
 
 public class AppContainer {
-    private static ViewModelFactory sFactory = null;
 
-    public static ViewModelFactory getInstance() {
-        if (sFactory == null) {
-            synchronized (ViewModelFactory.class) {
-                sFactory = new ViewModelFactory();
-            }
-        }
-        return sFactory;
-    }
-
-    public PlacesApi placesApi = new Retrofit.Builder()
+    public static PlacesApi placesApi = new Retrofit.Builder()
             .baseUrl(BASE_URL)
             .build()
             .create(PlacesApi.class);
 
-    private NearbySearchRepository getNearbySearchRepository() {
-        return new NearbySearchRepositoryImpl(placesApi);
-    }
+    private FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(Go4LunchApplication.getApplication());
+    private LocationTracker tracker = new DefaultLocationTracker(Go4LunchApplication.getApplication(), client);
+
+    private NearbySearchRepository nearbySearchRepository = new NearbySearchRepositoryImpl(placesApi);
+    public PlaceDetailsRepository placeDetailsRepository = new PlaceDetailsRepositoryImpl(placesApi);
 }
