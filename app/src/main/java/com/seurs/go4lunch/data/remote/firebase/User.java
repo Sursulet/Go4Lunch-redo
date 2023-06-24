fcb45b151fb733901acfd8eed0f6b5280664de71
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-24 22:30:45 +0200

    Create User.java

---
+++ app/src/main/java/com/seurs/go4lunch/data/remote/firebase/User.java
@@ -0,0 +1,41 @@
+package com.seurs.go4lunch.data.remote.firebase;
+
+import androidx.annotation.Nullable;
+
+public class User {
+    private String uid;
+    private String username;
+    @Nullable
+    private String avatarUrl;
+
+    public User(String uid, String username, @Nullable String avatarUrl) {
+        this.uid = uid;
+        this.username = username;
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Nullable
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(@Nullable String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+}
